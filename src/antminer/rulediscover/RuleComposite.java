a28c1d688f97bb94f48271b0b7752ebf8fe74231
retroq <retrozet@gmail.com>
2014-05-30 09:39:27 +0400

    Composite

---
+++ src/antminer/rulediscover/RuleComposite.java
@@ -0,0 +1,10 @@
+package antminer.rulediscover;
+
+/**
+ * Date: 21.04.14
+ * Time: 21:41
+ *
+ * @author anton
+ */
+public class RuleComposite {
+}
---
+++ src/naivebayes/Naive.java
@@ -0,0 +1,10 @@
+package naivebayes;
+
+/**
+ * Date: 26.04.14
+ * Time: 18:38
+ *
+ * @author anton
+ */
+public class Naive {
+}
